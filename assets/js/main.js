0b1b088f7e179eac8c7ee2068bd362647cc33837
Manu Kumar <heymanukumar@gmail.com>
2023-12-25 12:39:48 +0530

    contact_function

--- assets/js/main.js
+++ assets/js/main.js
@@ -35,24 +35,58 @@ let swiperProjects = new Swiper(".projects__container", {
     pagination: {
         el: ".swiper-pagination",
     },
-    breakpoints:{
-      1200:  {
-        slidePerView: 2,
-        spaceBetween: -56,
-      }
+    breakpoints: {
+        1200: {
+            slidePerView: 2,
+            spaceBetween: -56,
+        }
     }
 });
 /*=============== SWIPER TESTIMONIAL ===============*/
 let swiperTestimonial = new Swiper(".testimonial__container", {
-   grabCursor: true,
+    grabCursor: true,
     navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
     },
-  });
+});
 
 /*=============== EMAIL JS ===============*/
 
+const contactForm = document.getElementById('contact-form'),
+    contactName = document.getElementById('contact-name'),
+    contactEmail = document.getElementById('contact-email'),
+    contactProject = document.getElementById('contact-project'),
+    contactMessage = document.getElementById('contact-message'); // Fix typo here
+
+const sendEmail = (e) => {
+    e.preventDefault();
+
+    if (contactName.value === '' || contactEmail.value === '' || contactProject.value === '') {
+        // Add and remove color
+        contactMessage.classList.remove('color-blue');
+        contactMessage.classList.add('color-red');
+
+        // Show message
+        contactMessage.textContent = 'Write all the input fields';
+    } else {
+        // serviceID - templateID - #form - publicKey
+        emailjs.sendForm('service_jk0r56t', 'template_2vne68b', 'contact-for', 'EK3DEMnZ8S2VwyK4v')
+            .then(() => {
+                // Show success message and add color
+                contactMessage.classList.add('color-blue')
+                contactMessage.textContent = 'Message sent'
+
+                // Remove message after five seconds
+                setTimeout(() => {
+                    contactMessage.textContent = ''
+                }, 5000);
+            })
+
+    }
+};
+
+contactForm.addEventListener('submit', sendEmail);
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
